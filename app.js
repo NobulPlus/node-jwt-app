ea2fafb90ec6adb3ca2c04a07c16b350e2bda19e
NobulPlus <chenko4me@gmail.com>
2024-07-28 22:17:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes  = require('./routes/authRoutes');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://adeleyepaul2:NobulPlus94@jwt.yn1qql7.mongodb.net/?retryWrites=true&w=majority&appName=JWT'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then(() => {
+    app.listen(3000)
+    console.log("Database is connected");
+  }).catch((err) => {
+    console.error('Error connecting to database', err);
+  });
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
