1752890057caa3239c4e26b00fd62fb3dce36595
NobulPlus <chenko4me@gmail.com>
2024-07-29 00:49:04 +0100

    cookies

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes  = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser')
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -20,8 +22,20 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   }).catch((err) => {
     console.error('Error connecting to database', err);
   });
-
-// routes
+ // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+  res.cookie('newUser', false, { maxAge: 900000, httpOnly: true});
+  res.send('you get the cookies here');
+})
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+
+  res.json(cookies)
+})
